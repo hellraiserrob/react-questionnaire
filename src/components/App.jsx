879470940c6d8951ce8800fe7bea43fdd69e8cd6
hellraiserrob <robertjamesphillips@googlemail.com>
2017-01-08 09:34:10 +0000

    Fix tests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
@@ -8,37 +8,77 @@ import Footer from '../components/common/Footer'
 import './App.css'
 
 
+class App extends Component {
+	render(){
+
+		const children = this.props.children
+
+		return (
+
+			<div>
+
+				<Header />
+				
+				<div className="container">
+
+					<ReactCSSTransitionGroup	
+						transitionName="page"
+						transitionAppear={true}
+						transitionAppearTimeout={300}
+						transitionEnterTimeout={300}
+						transitionLeaveTimeout={500}
+						>
+						{React.cloneElement(children, {
+							key: location.pathname
+						})}
+						
+					</ReactCSSTransitionGroup>
+
+				</div>
+
+				<Footer />
+
+
+				
+			</div>
+
+
+		)
+	}
+}
+
+
 
 
 //import QuestionContainer from '../containers/questions/QuestionContainer';
 
-const App = ({children}) => (
-	<div>
+// const App = ({children}) => (
+// 	<div>
 
-		<Header />
+// 		<Header />
 		
-		<div className="container">
+// 		<div className="container">
 
-			<ReactCSSTransitionGroup	
-				transitionName="page"
-				transitionAppear={true}
-				transitionAppearTimeout={300}
-				transitionEnterTimeout={300}
-				transitionLeaveTimeout={500}
-				>
-				{React.cloneElement(children, {
-					key: location.pathname
-				})}
+// 			<ReactCSSTransitionGroup	
+// 				transitionName="page"
+// 				transitionAppear={true}
+// 				transitionAppearTimeout={300}
+// 				transitionEnterTimeout={300}
+// 				transitionLeaveTimeout={500}
+// 				>
+// 				{React.cloneElement(children, {
+// 					key: location.pathname
+// 				})}
 				
-			</ReactCSSTransitionGroup>
+// 			</ReactCSSTransitionGroup>
 
-		</div>
+// 		</div>
 
-		<Footer />
+// 		<Footer />
 
 
 		
-	</div>
-)
+// 	</div>
+// )
 
 export default App;
