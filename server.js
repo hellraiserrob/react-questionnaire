499ce25c6feb44aa03530c312f158fb4c785fdf5
hellraiserrob <robertjamesphillips@googlemail.com>
2017-01-16 13:12:43 +0000

    make a simple request for questions to mongoLab

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
-var express = require('express')
-var app = express()
+'use strict'
+
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const config = require('./config.js')
+let db
+
+MongoClient.connect(`mongodb://${config.username}:${config.pwd}@ds139278.mlab.com:39278/questionnaire`, (err, database) => {
+
+    if(err){
+        return console.log(err)
+    }
+
+    db = database
+
+    app.listen(3001, function(){
+        console.log('listening on 3001')
+    })
+
+})
+
 
 app.get('/', function(req, res){
-    res.send('testing...')
+    db.collection('questions').find().toArray((err, results) => {
+        res.send(results)
+    })
 })
 
-app.listen(3001, function(){
-    console.log('listening on port 3001....');
+app.get('/test', function(req, res){
+    
+    res.send({
+        'status': 'ok'
+    })
+    
 })
+
